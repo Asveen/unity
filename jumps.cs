408029e2f2000c8382490a5b048f7a73d33d104e
asveen <asveenk@gmail.com>
2018-01-03 15:25:09 +0530

    Game Development

---
+++ jumps.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumps : MonoBehaviour {
+
+	public GameObject cube;
+	public Vector3 jf;
+	// Use this for initialization
+	void Start () {
+		jf = new Vector3 (0f, 10f, 0f);
+		
+	}
+    void OnTriggerEnter()
+    {
+        print("Collision Detected");
+    }
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Jump")) {
+			cube.GetComponent<Rigidbody>().AddForce (jf);
+            
+		}
+	}
+}
